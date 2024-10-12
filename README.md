2439cafe94f622195ed6d6324cc71681b60f355b
LizNich <nicholsonliz@hotmail.com>
2024-10-12 16:45:54 +0100

    Submission

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # wk2-assignment
 
 Week 2 SD015 Assignment - Image Gallery with Accessible Features
+
+Reflection:
+
+Requirements achieved & not achieved:
+The webpage displays well and you are able to move from one picture to another using a mouse. I've not been able to work out how to do this using the left and right arrow keys and have left some code in that does not work.
+The pictures do no squash on smaller screens and the gallery boxes reformat neatly.
+Accessibility measures that do work are alt and pointer change.
+
+Further reflections:
+For the amount of time I've spent on this, it looks good, but without the left and right tab, or at one time my attempt to put a blue border around the image its lacking all the pieces needed.
+
+I'd appreciate some help with understanding why it hasn't worked.
+
+I googled, revisited the workshop recording and worked with fellow students on Friday. I also put my code into ChatGPT for help but it either wasn't useful or I didn't understand what it meant.
+
+Feedback welcome as always. Enjoy the peaceful pictures.
+Liz
--- script.js
+++ script.js
@@ -1,89 +1,63 @@
-//DOM do dars
+//DOM NODES
 const thumbnailContainer = document.getElementById("thumbnail-container");
 const displayImage = document.getElementById("display-image");
+let currentIndex = 0;
 
-//Array image store
+//ARRAY IMAGE STORE
 const images = [
   {
     src: "assets/02-pink.jpg",
     alt: "A peaceful pink japanese garden",
-    tabIndex: "0",
   },
   {
     src: "assets/03-purple.jpg",
     alt: "Purple and white clouds",
-    tabIndex: "0",
   },
   {
     src: "assets/04-blue.jpg",
     alt: "An artic blue sea, sky and iceberg",
-    tabIndex: "0",
   },
   {
     src: "assets/05-green.jpg",
     alt: "Green woodland sunlight",
-    tabIndex: "0",
   },
   {
     src: "assets/06-yellow.jpg",
     alt: "A yellow gerbera after the rain",
-    tabIndex: "0",
   },
   {
     src: "assets/07-orange.jpg",
     alt: "An orange garden table with coffee beans and mug",
-    tabIndex: "0",
   },
   {
     src: "assets/08-red.jpg",
     alt: "Two red tulips in front of a field of red tulips",
-    tabIndex: "0",
   },
   {
     src: "assets/09-black.jpg",
     alt: "A dark stary sky",
-    tabIndex: "0",
   },
 ];
 
+//IMAGE LOOP THROUGH + DISPLAY IMAGE
 for (let i = 0; i < images.length; i++) {
   const img = document.createElement("img");
-
   img.src = images[i].src;
   img.alt = images[i].alt;
-
   img.addEventListener("click", function () {
     displayImage.src = images[i].src;
     displayImage.alt = images[i].alt;
   });
   thumbnailContainer.appendChild(img);
-}
-//ABOVE IS FROM THE DEMO (+TabIndex)
-//
-//
-//does this work?
-// Select the image by its ID
-//const image = document.getElementById("clickableImage");
 
-// Add an event listener for the 'click' event
-//image.addEventListener("click", function () {
-// Toggle the 'red-border' class on click
-//image.classList.toggle("red-border");
-//});
-
-//document.addEventListener("keydown", function (event) {
-// if (event.code === "Enter") {
-//   document.querySelector("[tabindex='1']").focus();
-//  }
-// if (event.code === "Enter") {
-//   document.querySelector("[tabindex='2']").focus();
-// }
-//});
-
-function navigateLeftRight(event) {
-  if (event.key == "ArrowLeft") {
-    reverseImage();
-  } else if (event.key == "ArrowRight") {
-    advanceImage();
+  //why doesn't this work?
+  function handleKeyPress(event) {
+    if (event.key === "ArrowRight") {
+      // Move to the next image
+      currentIndex = currentIndex + 1;
+    } else if (event.key === "ArrowLeft") {
+      // Move to the previous image
+      currentIndex = currentIndex - 1 + images.length;
+    }
   }
 }
